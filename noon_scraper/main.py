ac499e67454a816ec5b2e62832d644bb82f1d068
TanatB <bright.tanat@hotmail.com>
2026-04-23 12:50:23 +0700

    Noon scraper init

---
+++ noon_scraper/main.py
@@ -0,0 +1,17 @@
+import httpx, asyncio
+from bs4 import BeautifulSoup
+from urllib.parse import urljoin
+from dataclasses import dataclass, field
+
+BASE_URL = "https://www.noon.com/uae-en/electronics-and-mobiles/computers-and-accessories/computers-new/laptops/"
+
+def noon_scraper():
+    pass
+
+
+def main():
+    print("Hello from noon-scraper!")
+
+
+if __name__ == "__main__":
+    main()
